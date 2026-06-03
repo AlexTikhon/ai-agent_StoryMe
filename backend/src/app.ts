efbdae9441bcaac623f1dbb46618be8a4ee2d80e
Alex Tikhon <optimalyo@gmail.com>
2026-06-03 15:09:38 +0200

    Add Helmet security middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
-﻿import cors from "cors";
+import cors from "cors";
 import express from "express";
+import helmet from "helmet";
 import { apiRouter } from "./routes";
 import { errorMiddleware } from "./middleware/error";
 
 export const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use("/api", apiRouter);
